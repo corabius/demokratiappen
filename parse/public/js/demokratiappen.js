24e3766fec10a7d20f222a93ec37aedd8a1b118a
Jens Rantil <jens.rantil@gmail.com>
2014-01-11 17:09:03 +0100

    Present all prepopulated tags

--- parse/public/js/demokratiappen.js
+++ parse/public/js/demokratiappen.js
@@ -3,7 +3,12 @@ LOGGED_IN = 1;
 
 angular.module('democracy-app', [])
 
-.factory('LoginService', function($rootScope) {
+.factory('ParseInitializer', function() {
+  Parse.initialize("amtD1gwFz83IOqSdVF6I2oaxZeqRJRE57nyj3dKY", 
+                   "24l3K1yDJpxkiYF4ZjUCtereM2jx9lET9LtKCvB4");
+})
+
+.factory('LoginService', function($rootScope, ParseInitializer) {
   var obj = {}
   obj.setLoginState = function(newState, message, messageClass) {
     obj.state = newState;
@@ -26,9 +31,8 @@ angular.module('democracy-app', [])
   return obj;
 })
 
-.controller('MainController', function() {
-  Parse.initialize("amtD1gwFz83IOqSdVF6I2oaxZeqRJRE57nyj3dKY", 
-                   "24l3K1yDJpxkiYF4ZjUCtereM2jx9lET9LtKCvB4");
+.controller('MainController', function($scope, LoginService) {
+  $scope.loginService = LoginService;
 })
 
 .controller('LoginController', function($scope, LoginService) {
@@ -37,7 +41,6 @@ angular.module('democracy-app', [])
   } else {
     $scope.mdLoginState = NOT_LOGGED_IN;
   }
-  $scope.loginService = LoginService;
   $scope.login = function() {
     Parse.User.logIn($scope.username, $scope.password, {
       success: function(user) {
@@ -66,6 +69,14 @@ angular.module('democracy-app', [])
     Parse.User.logOut();
     LoginService.setLoginState(NOT_LOGGED_IN, "Du har nu blivit utloggad.", "alert-success");
   }
+})
+
+.controller('AddController', function($scope, $http) {
+  //var Tag = Parse.Object.extend("Tag");
+  var query = new Parse.Query("Tag")
+  query.find().then(function(tags) {
+    $scope.tags = tags;
+  });
 });
 
 /*
