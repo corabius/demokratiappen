07c0d29654ed17f6f1312eb519fe334c95971ed6
Silvia Man <corabius@gmail.com>
2014-01-12 15:01:17 +0100

    created a new tab and added class LoginViewController with a button.

---
+++ iOS-client/DemokratiAppen/DemokratiAppen/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  DemokratiAppen
+//
+//  Created by Silvia Man on 2014-01-12.
+//  Copyright (c) 2014 Joakim Rydell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ iOS-client/DemokratiAppen/DemokratiAppen/LoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  LoginViewController.m
+//  DemokratiAppen
+//
+//  Created by Silvia Man on 2014-01-12.
+//  Copyright (c) 2014 Joakim Rydell. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@end
+
+@implementation LoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
