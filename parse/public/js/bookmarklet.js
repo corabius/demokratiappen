90a8d23fc4cffad04564adb810b726026680b58a
Erik Månsson <goodbyte@goodbyte.cjb.net>
2014-01-29 22:56:03 +0100

    Change server-adress in bookmarklet.js

--- parse/public/js/bookmarklet.js
+++ parse/public/js/bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:function iprl5(){var d=document,l=d.location;document.location=l.protocol+'//demokratiappen747.parseapp.com/#/?title='+d.title+'&url='+encodeURIComponent(l.href);}iprl5();void(0)
+javascript:function iprl5(){var d=document,l=d.location;document.location=l.protocol+'//demokratiappen.parseapp.com/#/?title='+d.title+'&url='+encodeURIComponent(l.href);}iprl5();void(0)
