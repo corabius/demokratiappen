9bb0d6db26e976c694627d376ff9ced7434de43e
Silvia Man <corabius@gmail.com>
2014-03-24 22:57:40 +0100

    added webViewController to show webb-plugins.

---
+++ iOS-client/DemokratiAppen/PluginWebViewController.h
@@ -0,0 +1,13 @@
+//
+//  PluginWebViewController.h
+//  DemokratiAppen
+//
+//  Created by Silvia Man on 2014-03-24.
+//  Copyright (c) 2014 Demokratiappen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PluginWebViewController : UIViewController
+
+@end
