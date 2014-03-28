4961bc1e5e1aa0e16b09ef6144b21cd6a55997e5
Silvia Man <corabius@gmail.com>
2014-03-28 16:12:52 +0100

    connected plugin webview to demokratiappen.se/mitt-konto.

--- iOS-client/DemokratiAppen/PluginWebViewController.m
+++ iOS-client/DemokratiAppen/PluginWebViewController.m
@@ -1,10 +1,21 @@
-//
-//  PluginWebViewController.m
-//  DemokratiAppen
-//
-//  Created by Silvia Man on 2014-03-24.
-//  Copyright (c) 2014 Demokratiappen. All rights reserved.
-//
+/* Copyright (C) 2014 Demokratiappen.
+ *
+ * This file is part of Demokratiappen.
+ *
+ * Demokratiappen is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Demokratiappen is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Demokratiappen.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 
 #import "PluginWebViewController.h"
 
@@ -28,7 +39,7 @@
 {
     [super viewDidLoad];
 
-    NSString *completeURL = @"http://demokratiappen.se";  //TODO: correct link
+    NSString *completeURL = @"http://demokratiappen.se/mitt-konto";  //TODO: connect without login again
     NSURL* url = [NSURL URLWithString: completeURL];
 
     NSURLRequest* urlRequest = [NSURLRequest requestWithURL: url];
